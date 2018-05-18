ac428fe0fb21a1a91a2a91781bbb3b7aa3b375ad
Mark Rowatt Anderson <rowatt@rowatt.com>
2018-05-18 17:16:45 +0100

    Add wysiwyg field counter & other changes

--- acf-input-counter.js
+++ acf-input-counter.js
@@ -1,43 +1,214 @@
+(function ($) {
 
-(function($) {
+	/**
+	 * Set maxlength data attribute of wysiwyg textarea field
+	 *
+	 * We don't use the maxlength attribute as that limits the length
+	 * in text mode to content including all HTML tags.
+	 *
+	 * @param e
+	 */
+	acf.set_wysiwyg_textarea_maxlength = function (e) {
+
+		var self = $(e.$field);
+		self.$el = self.find('.acf-input');
+
+		self.$textarea = self.$el.find('textarea');
+		var $parent = self.$el.parent();
+		var fieldkey = $parent.data('key');
+
+		if (!self.$el.data('maxlength')) {
+			/** @var object acf_input_counter_data set in HTML */
+			var maxlength = acf_input_counter_data[fieldkey];
+
+			self.$textarea.data('maxlength', maxlength);
+		}
+
+	};
+
+	/**
+	 * Change the counter
+	 *
+	 * @param content
+	 * @param $textarea
+	 */
+	acf.change_counter = function(content, $textarea) {
+		var counter = $textarea.closest('.acf-input').find('.count');
+		if (counter.length === 0) {
+			return;
+		}
+		var length = content.length;
+		counter.text(length);
+	};
+
+	/**
+	 * Get length of content after removing HTML tags so we are only counting
+	 * visible characters.
+	 */
+	acf.get_content_without_tags = function( content_html ) {
+		return acf.decode( content_html.replace(/<\/?[^>]+(>|$)/g, "") );
+	};
+
+	/**
+	 * Have we reached the max length of the wysiwyg textarea field
+	 *
+	 * @param e
+	 * @param content
+	 * @param $textarea
+	 * @returns {boolean}
+	 */
+	acf.wysiwyg_max_length_reached = function(e, content, $textarea) {
+		var allowed_keystroke = false;
+		var maxlength = $textarea.data('maxlength');
+
+		//maxlength not defined see acf.set_wysiwyg_textarea_maxlength() below
+		if (typeof (maxlength) === 'undefined') return;
+
+		var length = acf.get_content_without_tags( content ).length;
+
+		var valid_keycodes = [
+			8, //backspace
+			33, //home
+			34, //end
+			35, //pagedown
+			36, //pageup
+			37, //left
+			38, //up
+			39, //right
+			40, //down
+			46 //delete
+		];
+
+		//windows - ctrlKey = control key
+		//mac - metaKey = cmd key
+		var cmd = ( e.ctrlKey || e.metaKey );
+
+		//never prevent delete keys being used
+		if ( $.inArray( e.keyCode, valid_keycodes ) > -1
+			|| ( cmd && e.keyCode === 88) // cut
+			|| ( cmd && e.keyCode === 67) // ctrl+c
+		) {
+			allowed_keystroke = true;
+		}
+
+		return ( ! allowed_keystroke && length >= maxlength );
+	};
+
+	/**
+	 * Add tinyMCE events for counting & limiting content length
+	 * of wysiwyg field in visual mode
+	 */
+	acf.add_filter('wysiwyg_tinymce_settings', function (init, id) {
+
+		//extend parent init.setup() method
+		var _setup = init.setup;
+		init.setup = function(editor) {
+
+			/**
+			 * Limit length to maxlength
+			 */
+			editor.on('keyDown', function(e) {
+				var content = editor.getContent();
+				var $textarea = $(editor.getElement());;
+
+				//don't allow typing after maxlength
+				if( acf.wysiwyg_max_length_reached(e, content, $textarea ) ) {
+					tinymce.dom.Event.cancel(e);
+				}
+			});
+
+			/*
+			 * Update character counter
+			 */
+			editor.on('keyUp', function () {
+				var content = acf.get_content_without_tags( editor.getContent() );
+				var $textarea = $(editor.getElement());
+				acf.change_counter( content, $textarea );
+			});
+
+			//run parent method
+			return _setup.apply(this, arguments);
+		};
+
+		return init;
+	});
+
+	/**
+	 * Setup events for text field counter
+	 */
 	acf.fields.text_counter = acf.field.extend({
 		type: 'text',
-		
 		events: {
 			'input input': 'change_count',
+			'focus input': 'change_count'
 		},
-		
-		change_count: function(e){
-			var $max = e.$el.attr('maxlength');
-			if (typeof($max) == 'undefined' || e.$el.closest('.acf-input').find('.count').length == 0) {
-				return;
-			}
-			var $value = e.$el.val();
-			var $length = $value.length;
-			e.$el.closest('.acf-input').find('.count').text($length);
-		},
-		
+		change_count: function (e) {
+			var content = e.$el.val();
+			var $textarea = e.$el;
+			acf.change_counter( content, $textarea );
+		}
 	});
-})(jQuery);
 
-
-(function($) {
+	/**
+	 * Setup events for textarea field counter
+	 */
 	acf.fields.textarea_counter = acf.field.extend({
 		type: 'textarea',
-		
 		events: {
 			'input textarea': 'change_count',
+			'focus textarea': 'change_count'
 		},
-		
-		change_count: function(e){
-			var $max = e.$el.attr('maxlength');
-			if (typeof($max) == 'undefined') {
-				return;
-			}
-			var $value = e.$el.val();
-			var $length = $value.length;
-			e.$el.closest('.acf-input').find('.count').text($length);
-		},
-		
+		change_count: function(e) {
+			var content = e.$el.val();
+			var $textarea = e.$el;
+			acf.change_counter( content, $textarea );
+		}
+
 	});
+
+	/**
+	 * Setup events for wysiwyg counter
+	 */
+	acf.fields.wysiwyg_counter = acf.field.extend({
+		type: 'wysiwyg',
+		actions: {
+			'load': 'set_wysiwyg_textarea_maxlength'
+		},
+		set_wysiwyg_textarea_maxlength: function () {
+			acf.set_wysiwyg_textarea_maxlength(this)
+		},
+
+		//events are for wysiwyg field in text mode
+		events: {
+			'input .wp-editor-area': 'change_count',
+			'focus .wp-editor-area': 'change_count',
+			'keydown .wp-editor-area': 'limit_chars',
+			'click .wp-media-buttons button': 'check_editor'
+		},
+
+		//change the counter
+		change_count: function (e) {
+			var content = acf.get_content_without_tags( e.$el.val() );
+			var $textarea = e.$el;
+			acf.change_counter( content, $textarea );
+		},
+
+		//limit chars to maxlength
+		limit_chars: function (e) {
+			var content = acf.get_content_without_tags( e.$el.val() );
+			var $textarea = e.$el;
+
+			//don't allow typing after maxlength
+			if( acf.wysiwyg_max_length_reached(e, content, $textarea ) ) {
+				e.preventDefault();
+			}
+		},
+
+		//force triggering "change" event on tinymce/quicktags switching
+		check_editor: function () {
+			$(tinyMCE.activeEditor.targetElm).trigger('change').focus();
+		}
+
+	});
+
 })(jQuery);
--- acf-input-counter.php
+++ acf-input-counter.php
@@ -1,188 +1,312 @@
 <?php
 
-	/*
-		Plugin Name: ACF Input Counter
-		Plugin URI: https://github.com/Hube2/acf-input-counter/
-		Description: Show character count for limited text and textarea fields
-		Version: 1.4.1
-		Author: John A. Huebner II
-		Author URI: https://github.com/Hube2/
-		Text-domain: acf-counter
-		Domain-path: languages
-		GitHub Plugin URI: https://github.com/Hube2/acf-input-counter/
-		License: GPL
-	*/
+/*
+	Plugin Name: ACF Input Counter
+	Plugin URI: https://github.com/Hube2/acf-input-counter/
+	Description: Show character count for limited text and textarea fields
+	Version: 1.4.1
+	Author: John A. Huebner II
+	Author URI: https://github.com/Hube2/
+	Text-domain: acf-counter
+	Domain-path: languages
+	GitHub Plugin URI: https://github.com/Hube2/acf-input-counter/
+	License: GPL
+*/
 
-	// If this file is called directly, abort.
-	if (!defined('WPINC')) {die;}
+// If this file is called directly, abort.
+if (!defined('WPINC')) {die;}
 
-	new acf_input_counter();
+new acf_input_counter();
 
-	class acf_input_counter {
+class acf_input_counter {
 
-		private $version = '1.4.1';
+	private $version = '1.4.1';
 
-		public function __construct() {
-			add_action('plugins_loaded', 					array($this, 'acf_counter_load_plugin_textdomain'));
-			add_action('acf/render_field/type=text', 		array($this, 'render_field'), 20, 1);
-			add_action('acf/render_field/type=textarea', 	array($this, 'render_field'), 20, 1);
-			add_action('acf/input/admin_enqueue_scripts', 	array($this, 'scripts'));
-			add_filter('jh_plugins_list', 					array($this, 'meta_box_data'));
-		} // end public function __construct
+	/**
+	 * Field types which have character limits
+	 *
+	 * @var array
+	 */
+	private $limited_field_types = [
+		'text',
+		'textarea',
+		'wysiwyg'
+	];
 
-		public function acf_counter_load_plugin_textdomain() {
-	    	load_plugin_textdomain( 'acf-counter', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	public function __construct() {
+
+		add_action('plugins_loaded', [ $this, '_acf_counter_load_plugin_textdomain' ] );
+		add_action('acf/input/admin_enqueue_scripts', [ $this, '_scripts' ] );
+		add_filter('jh_plugins_list', [ $this, '_meta_box_data' ] );
+
+		foreach ($this->limited_field_types as $type) {
+			//adds counter beneath fields
+			add_action('acf/render_field/type=' . $type, [ $this, '_render_field' ], 20, 1);
+			//validates field length
+			add_filter('acf/validate_value/type=' . $type, [ $this, '_validate_maxlength' ], 10, 4 );
 		}
 
-			function meta_box_data($plugins=array()) {
+		//wysiwyg field specific hook
+		add_action('acf/render_field_settings/type=wysiwyg', [ $this, '_wysiwyg_field_settings' ], 10, 1);
 
-				$plugins[] = array(
-					'title' 	=> 'ACF Input Counter',
-					'screens' 	=> array('acf-field-group', 'edit-acf-field-group'),
-					'doc' 		=> 'https://github.com/Hube2/acf-input-counter'
-				);
-				return $plugins;
+	}
 
-			} // end function meta_box
+	public function _acf_counter_load_plugin_textdomain() {
+		load_plugin_textdomain( 'acf-counter', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	}
 
-		private function run() {
-			// cannot run on field group editor or it will
-			// add code to every ACF field in the editor
-			$run = true;
-			global $post;
-			if ($post && $post->ID && get_post_type($post->ID) == 'acf-field-group') {
-				$run = false;
-			}
-			return $run;
-		} // end private function run
+	public function _meta_box_data( $plugins=[] ) {
 
-		public function scripts() {
-			if (!$this->run()) {
-				return;
-			}
-			// wp_enqueue_script
-			$handle    	= 'acf-input-counter';
-			$src       	= plugin_dir_url(__FILE__).'acf-input-counter.js';
-			$deps      	= array('acf-input');
-			$ver       	= $this->version;
-			$in_footer 	= false;
-			wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
+		$plugins[] = array(
+			'title' 	=> 'ACF Input Counter',
+			'screens' 	=> array('acf-field-group', 'edit-acf-field-group'),
+			'doc' 		=> 'https://github.com/rowatt/acf-input-counter'
+		);
+
+		return $plugins;
+	}
+
+	/**
+	 * Can we run?
+	 *
+	 * We cannot run on field group editor as it will
+	 * add code to every ACF field in the editor
+	 *
+	 * @return bool
+	 */
+	private function run() {
+		$run = true;
+		global $post;
+		if ($post && $post->ID && get_post_type($post->ID) == 'acf-field-group') {
+			$run = false;
+		}
+
+		return $run;
+	}
+
+	/**
+	 * Enqueue scripts and stylesheets
+	 */
+	public function _scripts() {
+		if (!$this->run()) {
+			return;
+		}
+
+		$handle    	= 'acf-input-counter';
+		$src       	= plugin_dir_url(__FILE__).'acf-input-counter.js';
+		$deps      	= array('acf-input');
+		$ver       	= $this->version;
+		$in_footer 	= false;
+		wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
+
+		$data = [
+			'init' => true
+		];
+		wp_localize_script( $handle, 'acf_input_counter_data', $data );
+
+		/**
+		 * Turn off plugin CSS if you want to incorporate into site stylesheet
+		 *
+		 * @param bool
+		 */
+		if( apply_filters( 'acf-input-counter/load-css', true ) ) {
 			wp_enqueue_style('acf-counter', plugins_url( 'acf-counter.css' , __FILE__ ));
-		} // end public function scripts
-
-		public function render_field($field) {
-			//echo '<pre>'; print_r($field); echo '</pre>';
-			if (!$this->run() ||
-			    !$field['maxlength'] ||
-			    ($field['type'] != 'text' && $field['type'] != 'textarea')) {
-				// only run on text and text area fields when maxlength is set
-				return;
-			}
-			if (function_exists('mb_strlen')) {
-				$len = mb_strlen($field['value']);
-			} else {
-				$len = strlen($field['value']);
-			}
-			$max = $field['maxlength'];
-
-			$classes 	= apply_filters('acf-input-counter/classes', array());
-			$ids 		= apply_filters('acf-input-counter/ids', array());
-
-			$insert = true;
-			if (count($classes) || count($ids)) {
-				$insert = false;
-
-				$exist = array();
-				if ($field['wrapper']['class']) {
-					$exist = explode(' ', $field['wrapper']['class']);
-				}
-				$insert = $this->check($classes, $exist);
-
-				if (!$insert && $field['wrapper']['id']) {
-					$exist = array();
-					if ($field['wrapper']['id']) {
-						$exist = explode(' ', $field['wrapper']['id']);
-					}
-					$insert = $this->check($ids, $exist);
-				}
-			} // end if filter classes or ids
-
-			if (!$insert) {
-				return;
-			}
-			$display = sprintf(
-				__('chars: %1$s of %2$s', 'acf-counter'),
-				'%%len%%',
-				'%%max%%'
-			);
-			$display = apply_filters('acf-input-counter/display', $display);
-			$display = str_replace('%%len%%', '<span class="count">'.$len.'</span>', $display);
-			$display = str_replace('%%max%%', $max, $display);
-			?>
-				<span class="char-count">
-					<?php
-						echo $display;
-					?>
-				</span>
-			<?php
-		} // end public function render_field
-
-		private function check($allow, $exist) {
-			// if there is anything in $allow
-			// see if any of those values are in $exist
-			$intersect = array_intersect($allow, $exist);
-			if (count($intersect)) {
-				return true;
-			}
-			return false;
-		} // end private function check
-
-	} // end class acf_input_counter
-
-	if (!function_exists('jh_plugins_list_meta_box')) {
-		function jh_plugins_list_meta_box() {
-			if (apply_filters('remove_hube2_nag', false)) {
-				return;
-			}
-			$plugins = apply_filters('jh_plugins_list', array());
-
-			$id 		= 'plugins-by-john-huebner';
-			$title 		= '<a style="text-decoration: none; font-size: 1em;" href="https://github.com/Hube2" target="_blank">Plugins by John Huebner</a>';
-			$callback 	= 'show_blunt_plugins_list_meta_box';
-			$screens 	= array();
-			foreach ($plugins as $plugin) {
-				$screens = array_merge($screens, $plugin['screens']);
-			}
-			$context 	= 'side';
-			$priority 	= 'low';
-			add_meta_box($id, $title, $callback, $screens, $context, $priority);
-
-
-		} // end function jh_plugins_list_meta_box
-		add_action('add_meta_boxes', 'jh_plugins_list_meta_box');
-
-		function show_blunt_plugins_list_meta_box() {
-			$plugins = apply_filters('jh_plugins_list', array());
-			?>
-				<p style="margin-bottom: 0;">Thank you for using my plugins</p>
-				<ul style="margin-top: 0; margin-left: 1em;">
-					<?php
-						foreach ($plugins as $plugin) {
-							?>
-								<li style="list-style-type: disc; list-style-position:">
-									<?php
-										echo $plugin['title'];
-										if ($plugin['doc']) {
-											?> <a href="<?php echo $plugin['doc']; ?>" target="_blank">Documentation</a><?php
-										}
-									?>
-								</li>
-							<?php
-						}
-					?>
-				</ul>
-				<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hube02%40earthlink%2enet&lc=US&item_name=Donation%20for%20WP%20Plugins%20I%20Use&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">Please consider making a small donation.</a></p><?php
 		}
-	} // end if !function_exists
+	}
 
-?>
+	/**
+	 * Add max length option to wysiwyg field
+	 *
+	 * @param $field
+	 */
+	public function _wysiwyg_field_settings( $field ) {
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Character Limit','acf'),
+			'instructions'	=> __('Leave blank for no limit','acf'),
+			'type'			=> 'number',
+			'name'			=> 'maxlength',
+		));
+
+	}
+
+	/**
+	 * Add character counter when rendering limited field
+	 *
+	 * @param array $field
+	 */
+	public function _render_field( $field ) {
+
+		//only run on field types we are limiting which have maxlength set
+		if ( ! $this->run() ||
+		     ! isset( $field[ 'maxlength' ] ) ||
+		     ! in_array( $field['type'], $this->limited_field_types ) ) {
+			return;
+		}
+
+		$len = $this->content_length( $field[ 'value' ] );
+		$max = $field[ 'maxlength' ] ?? 0;
+
+		$classes = apply_filters( 'acf-input-counter/classes', array() );
+		$ids     = apply_filters( 'acf-input-counter/ids', array() );
+
+		$insert = TRUE;
+		if ( count( $classes ) || count( $ids ) ) {
+
+			$exist = [];
+			if ( $field[ 'wrapper' ][ 'class' ] ) {
+				$exist = explode( ' ', $field[ 'wrapper' ][ 'class' ] );
+			}
+			$insert = $this->does_allowed_exist( $classes, $exist );
+
+			if ( ! $insert && $field[ 'wrapper' ][ 'id' ] ) {
+				$exist = array();
+				if ( $field[ 'wrapper' ][ 'id' ] ) {
+					$exist = explode( ' ', $field[ 'wrapper' ][ 'id' ] );
+				}
+				$insert = $this->does_allowed_exist( $ids, $exist );
+			}
+		}
+
+		if ( ! $insert ) {
+			return;
+		}
+
+		//output wysiwyg maxlength - used by counter js
+		if( $max && 'wysiwyg' == $field['type'] ) {
+			printf( '<script>acf_input_counter_data.%s="%d";</script>', $field['key'], $max );
+		}
+
+		$display = sprintf(
+			__( 'chars: %1$s of %2$s', 'acf-counter' ),
+			'%%len%%',
+			'%%max%%'
+		);
+		/**
+		 * Filter the text format of the character counter
+		 *
+		 * String should contain:
+		 * 	%%len%% - replaced with number of characters
+		 * 	%%max%% - replaced with max allowed number of characters
+		 *
+		 * @param string text to display
+		 */
+		$display = apply_filters( 'acf-input-counter/display', $display );
+
+		$display = str_replace( '%%len%%', '<span class="count">' . $len . '</span>', $display );
+		$display = str_replace( '%%max%%', $max, $display );
+		if ( isset( $field[ 'maxlength' ] ) && $field[ 'maxlength' ] > 0 ) {
+			printf( '<span class="char-count">%s</span>',$display );
+		}
+
+	}
+
+	/**
+	 * Do any elements present in an array match at least
+	 * one element in the allowed list?
+	 *
+	 * @param array $allow elements that are allowed
+	 * @param array $exist elements that are present
+	 *
+	 * @return bool
+	 */
+	private function does_allowed_exist($allow, $exist) {
+		$intersect = array_intersect($allow, $exist);
+		if (count($intersect)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Make sure that fields can't be more than max length
+	 *
+	 * @param $valid
+	 * @param $value
+	 * @param $field
+	 * @param $input
+	 *
+	 * @return string
+	 */
+	public function _validate_maxlength( $valid, $value, $field, $input ) {
+
+		$maxlength = $field['maxlength'] ?? 0;
+
+		if( $maxlength ) {
+			$content_length = $this->content_length( $value );
+			if ( $content_length > $maxlength ) {
+				$msg = __( 'Field must be no more than %d characters', 'acf-counter' );
+
+				return sprintf( $msg, $maxlength );
+			}
+		}
+
+		return $valid;
+	}
+
+	/**
+	 * Get length of content after stripping out HTML and other things
+	 *
+	 * @param $content
+	 *
+	 * @return int content length
+	 */
+	private function content_length( $content ) {
+		//post_content can include HTML tags, so make sure we strip those out, remove double spaces etc
+		//and convert any HTML entities to their single unicode character
+		$content  = strip_tags( $content );
+		$content  = preg_replace( '#[\s]{2,}#', ' ', $content );
+		$content  = html_entity_decode( $content, ENT_HTML5 );
+
+		return mb_strlen( $content );
+	}
+
+}
+
+if (!function_exists('jh_plugins_list_meta_box')) {
+
+	function jh_plugins_list_meta_box() {
+		if (apply_filters('remove_hube2_nag', false)) {
+			return;
+		}
+		$plugins = apply_filters('jh_plugins_list', array());
+
+		$id 		= 'plugins-by-john-huebner';
+		$title 		= '<a style="text-decoration: none; font-size: 1em;" href="https://github.com/Hube2" target="_blank">Plugins by John Huebner</a>';
+		$callback 	= 'show_blunt_plugins_list_meta_box';
+		$screens 	= array();
+		foreach ($plugins as $plugin) {
+			$screens = array_merge($screens, $plugin['screens']);
+		}
+		$context 	= 'side';
+		$priority 	= 'low';
+		add_meta_box($id, $title, $callback, $screens, $context, $priority);
+	}
+	add_action('add_meta_boxes', 'jh_plugins_list_meta_box');
+
+	function show_blunt_plugins_list_meta_box() {
+		$plugins = apply_filters('jh_plugins_list', array());
+		?>
+			<p style="margin-bottom: 0;">Thank you for using my plugins</p>
+			<ul style="margin-top: 0; margin-left: 1em;">
+				<?php
+					foreach ($plugins as $plugin) {
+						?>
+							<li style="list-style-type: disc; list-style-position:">
+								<?php
+									echo $plugin['title'];
+									if ($plugin['doc']) {
+										?> <a href="<?php echo $plugin['doc']; ?>" target="_blank">Documentation</a><?php
+									}
+								?>
+							</li>
+						<?php
+					}
+				?>
+			</ul>
+			<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hube02%40earthlink%2enet&lc=US&item_name=Donation%20for%20WP%20Plugins%20I%20Use&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">Please consider making a small donation.</a></p><?php
+	}
+}
+
+/* EOF */
